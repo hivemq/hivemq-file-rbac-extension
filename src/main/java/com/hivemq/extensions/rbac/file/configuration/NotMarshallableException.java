f705716b07f1abfeb38db4355aaa580f01485bbd
David Sondermann <david.sondermann@hivemq.com>
2026-03-04 12:00:06 +0100

    Replace license plugin with Spotless 8.3.0

--- src/main/java/com/hivemq/extensions/rbac/file/configuration/NotMarshallableException.java
+++ src/main/java/com/hivemq/extensions/rbac/file/configuration/NotMarshallableException.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.extensions.rbac.file.configuration;
 
 import org.jetbrains.annotations.NotNull;
--- src/main/java/com/hivemq/extensions/rbac/file/utils/Hashing.java
+++ src/main/java/com/hivemq/extensions/rbac/file/utils/Hashing.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.hivemq.extensions.rbac.file.utils;
 
 import org.bouncycastle.crypto.digests.SHA512Digest;
